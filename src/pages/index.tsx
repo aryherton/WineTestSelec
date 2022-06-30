2afd0cc5ee9a1f74bf9171c4ca2cb07e29e95531
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-06-29 22:02:01 -0300

    fix: removed tag "meta"

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Wine Project</title>
+      </Head>
+
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home;
