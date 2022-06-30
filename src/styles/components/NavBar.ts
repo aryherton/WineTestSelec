b4373be7714bba40664ae02555de8f6396c3cade
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-06-30 03:54:23 -0300

    feat: inserted tag "a" in file "./src/styles/components/NavBar.ts"

--- src/styles/components/NavBar.ts
+++ src/styles/components/NavBar.ts
@@ -16,5 +16,12 @@ export const NavBarWrapper = styled.div`
     li {
         height: 100%;
     }
+    a {
+      color: ${props => props.theme.colors.textNavBar};
+      text-decoration: none;
+    }
+    a:hover {
+      color: ${props => props.theme.colors.textNavBarHover};
+    }
   }
 `;
