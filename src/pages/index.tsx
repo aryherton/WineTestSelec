4996c0768ed1c3e89a2c3e808f7a5611d3faf1b9
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-06-30 13:36:33 -0300

    feat: inserted require "axios" by funciton "getArr" in file "./src/pages/index.tsx"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,27 @@
-import Head from 'next/head'
+import { useEffect, useCallback, useState } from 'react';
+
+import Header from '../components/Header';
+import { HomeWrapper } from '../styles/pages/Home';
+import { getArr } from '../services/api';
+import Url from '../help/Url';
 
 const Home: React.FC = () => {
-  return (
-    <div>
-      <Head>
-        <title>Wine Project</title>
-      </Head>
+  const [product, setProduct] = useState('');
+  const data = useCallback(async () => {
+    const arrProduct = await getArr(Url.ENDPOINT);
+    setProduct(arrProduct);
+  }, []);
+  console.log(product);
+  useEffect(() => {
+    data();
+  }, [data]);
 
+  return (
+    <HomeWrapper>
+      <Header />
       <main>
-        <h1>Hello World</h1>
       </main>
-    </div>
+    </HomeWrapper>
   )
 }
 
