def110bd45a28c441572cad6c949e8047cadb670
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-06-30 13:34:43 -0300

    feat: inserted "Link" in file "./src/components/NavBar.tsx"

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,36 @@
+import Link from 'next/link';
+
 import { NavBarWrapper } from '../styles/components/NavBar';
 
 export default function NavBar() {
   return (
     <NavBarWrapper id="divNavBar">
       <ul id="ulNavBar">
-        <li>Clube</li>
-        <li>Loja</li>
-        <li>Produtores</li>
-        <li>Ofertas</li>
-        <li>Eventos</li>
+        <Link href="/">
+          <a>
+            <li>Clube</li>
+          </a>
+        </Link>
+        <Link href="/">
+          <a>
+            <li>Loja</li>
+          </a>
+        </Link>
+        <Link href="/">
+          <a>
+            <li>Produtores</li>
+          </a>
+        </Link>
+        <Link href="/">
+          <a>
+            <li>Ofertas</li>
+          </a>
+        </Link>
+        <Link href="/">
+          <a>
+            <li>Eventos</li>
+          </a>
+        </Link>
       </ul>
     </NavBarWrapper>
   );
