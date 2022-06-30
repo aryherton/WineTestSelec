5569f5d2965d993156edf8e2b6e72af4917a6ef4
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-06-30 03:02:27 -0300

    feat: created file "Header.ts" in folder "./src/styles/components"

---
+++ src/styles/components/Header.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 88.23px;
+  background-color: ${props => props.theme.colors.backgroundCard};
+
+  #logo_wine {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 40%;
+  }
+
+  h1 {
+    color: ${props => props.theme.colors.textPrimary};
+  }
+`;
