dfde0efdc26f314d8fdf65a599a242157758e1bc
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-01 01:50:01 -0300

    feat: created file "SearchPrice.ts" in folder "./src/styles/components"

---
+++ src/styles/components/SearchPrice.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const SearchByPriceWrapper = styled.div`
+  width: 25%;
+  height: 300px;
+  background-color: blue;
+
+  #divSpanSearchPrice {
+      display: flex;
+      flex-direction: column;
+      align-content: space-around;
+      justify-items: center;
+    }
+`;
