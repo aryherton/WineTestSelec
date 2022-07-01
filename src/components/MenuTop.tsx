5cb318a247901019b5e55e5638745a2af7d9243e
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-01 04:33:08 -0300

    feat: import "useEffect, useDispatch, useSelector" in file "./src/components/MenuTop.tsx"

--- src/components/MenuTop.tsx
+++ src/components/MenuTop.tsx
@@ -1,26 +1,47 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Link from 'next/link';
 import Image from 'next/image';
 
 import { MenuTopWrapper } from '../styles/components/MenuTop';
+
 import lupa from '../images/buscaLupa.png';
 import conta from '../images/conta.png';
 import winebox from '../images/winebox.png';
+import { setCheckSearch } from '../store/slice/productSlice';
 
 export default function MenuTop() {
+  const statusSearch = useSelector((state: any) => state.products.checkSearch);
+  const dispatch = useDispatch();
+  
+  const validSearch = () => {
+    const check = statusSearch ? false : true;
+    dispatch(setCheckSearch(check));
+  };
+
   return (
-    <MenuTopWrapper id="divMenuTop">
+    <MenuTopWrapper>
       <ul id="ulMenuTop">
-        <li>
-          <Image
-            src={lupa}
-            alt="lupaIcon"
-          />
-        </li>
-        <li>
-          <Image
-            src={conta}
-            alt="contaIcon"
-          />
-        </li>
+        <Link href="/">
+          <a onClick={ validSearch }>
+            <li>
+              <Image
+                src={lupa}
+                alt="lupaIcon"
+              />
+            </li>
+          </a>
+        </Link>
+        <Link href="/">
+          <a>
+            <li>
+              <Image
+                src={conta}
+                alt="contaIcon"
+              />
+            </li>
+          </a>
+        </Link>
         <li id="liWineBox">
           <Image
             id="imgWineBox"
