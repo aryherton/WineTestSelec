2cdae51ffcd56a71d04f91ef3d306538edc8f1f4
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-01 01:45:32 -0300

    feat: import "SearchByPrice" in file "./src/pages/index.tsx"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 
 import Header from '../components/Header';
 import CardProduct from '../components/CardProduct';
+import SearchByPrice from '../components/SearchByPrice';
 
 import { HomeWrapper } from '../styles/pages/Home';
 import { getArr } from '../services/api';
@@ -15,7 +16,7 @@ const Home: React.FC = () => {
     const arrProduct = await getArr(Url.ENDPOINT);
 
     dispatch(setProducts(arrProduct));
-  }, []);  
+  }, []);
 
   useEffect(() => {
     getData();
@@ -25,6 +26,7 @@ const Home: React.FC = () => {
     <HomeWrapper>
       <Header />
       <main>
+        <SearchByPrice />
         <CardProduct />
       </main>
     </HomeWrapper>
