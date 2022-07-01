08aeef958eb36520e352fa773a1c2646f8525b72
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-01 02:32:05 -0300

    feat: add css "#textRedifineBusca, #textPorPreco, #divUSearchPrice" in file "./src/styles/ components/SearchPrice.ts"

--- src/styles/components/SearchPrice.ts
+++ src/styles/components/SearchPrice.ts
@@ -2,13 +2,49 @@ import styled from 'styled-components';
 
 export const SearchByPriceWrapper = styled.div`
   width: 25%;
-  height: 300px;
-  background-color: blue;
+  height: 310px;
+  padding-left: 30px;
+  padding-top: 20px;
 
   #divSpanSearchPrice {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    justify-items: center;
+    margin-bottom: 20px;
+    width: 100%;
+    height: 20%;
+
+    #textRefineBusca {
+      color: #000000;
+      font-size: 20px;
+      font-weight: bolder;
+    }
+
+    #textPorPreco {
+      font-size: 18px;
+      color: #333333;
+      font-weight: bolder;
+    }
+  }
+
+  #divUlSearchPrice {
+    display: flex;
+    width: 249px;
+    height: 150px;
+
+    label {
+      padding-left: 10px;
+    }
+    
+    ul {
+      list-style: none;
+      color: #1D1D1B;
+      font-size: 14px;
+      font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
       display: flex;
       flex-direction: column;
-      align-content: space-around;
-      justify-items: center;
+      justify-content: space-evenly;
     }
+  }
 `;
