c07e9aecfc421a3801e1a56a8cadb327908fd15b
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-01 04:33:54 -0300

    feat: created file "Search.tsx" in folder "./src/components"

---
+++ src/components/Search.tsx
@@ -0,0 +1,9 @@
+import { SearchWrapper } from '../styles/components/Search';
+
+export default function Search() {
+  return (
+    <SearchWrapper id="inputSearch">
+      <input type="text" placeholder="Pesquise pelo nome"/>
+    </SearchWrapper>
+  );
+}
