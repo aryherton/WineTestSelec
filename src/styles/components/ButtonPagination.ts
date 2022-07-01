b8cb177552ba3dfa807c3ad9d86fb1ebea075da4
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-01 06:06:13 -0300

    feat: created file "ButtonPagination.ts" in folder "./src/styles/components"

---
+++ src/styles/components/ButtonPagination.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonPaginationWrapper = styled.div`
+  width: 90%;
+  height: 60px;
+  display: flex;
+  justify-content: space-evenly;
+
+  input {
+    width: 90px;
+    height: 90px;
+    background-color: #c40f73;
+    border-radius: 50%;
+  }
+`;
