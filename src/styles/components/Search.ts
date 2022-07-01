9fce2e2f752192159d3a516379679c49192391dd
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-01 04:37:23 -0300

    faet: created file "Search.ts" in folder "./src/styles/components"

---
+++ src/styles/components/Search.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const SearchWrapper = styled.div`
+  input {
+    position: absolute;
+    margin-top: 22px;
+    margin-left: 47%;
+    width: 380px;
+    height: 45px;
+    background-color: #F79552;
+    border-radius: 3%;
+  }
+`;
