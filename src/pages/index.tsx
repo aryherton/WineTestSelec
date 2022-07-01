8c64871eb9eadd5cbf999e15d5ddded8553a4d59
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-06-30 22:42:33 -0300

    feat: inserted components "CardProduct" in file "./src/pages/index.tsx"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import Header from '../components/Header';
+import CardProduct from '../components/CardProduct';
+
 import { HomeWrapper } from '../styles/pages/Home';
 import { getArr } from '../services/api';
 import Url from '../help/Url';
@@ -13,11 +15,7 @@ const Home: React.FC = () => {
     const arrProduct = await getArr(Url.ENDPOINT);
 
     dispatch(setProducts(arrProduct));
-  }, []);
-  
-  const { products } = useSelector((state: any) => state.products);
-  console.log('Global', products.items,'Global');
-  
+  }, []);  
 
   useEffect(() => {
     getData();
@@ -27,6 +25,7 @@ const Home: React.FC = () => {
     <HomeWrapper>
       <Header />
       <main>
+        <CardProduct />
       </main>
     </HomeWrapper>
   )
