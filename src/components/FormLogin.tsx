3c9f7e5a3daa4d696a9de2296b4743f9b476d138
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-01 21:19:55 -0300

    feat: created file "FormLogin.tsx" in folder "./src/components/FormaLogin.tsx"

---
+++ src/components/FormLogin.tsx
@@ -0,0 +1,81 @@
+import React, { useState, } from 'react';
+
+import { FormLoginWrapper } from '../styles/components/FormLogin';
+
+export default function FormLogin() {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [inputSubmit, setInputSubmit] = useState({
+    valueSubmit: 'Entrar',
+    valueTxtLink: 'Registre-se'
+  });
+
+  const getName = (event) => {
+    setUser({
+      ...user,
+      name: event.target.value,
+    });
+  }
+
+  const getEmail = (event) => {
+    setUser({
+      ...user,
+      email: event.target.value,
+    });
+  }
+
+  const getPassword = (event) => {
+    setUser({
+      ...user,
+      password: event.target.value,
+    });
+  }
+
+  const setValueSubmit = async () => {    
+    if (inputSubmit.valueTxtLink === 'Registre-se') {
+      setInputSubmit({
+        valueSubmit: 'Registrar',
+        valueTxtLink: 'Login'
+      });
+    } else {
+      setInputSubmit({
+        valueSubmit: 'Entrar',
+        valueTxtLink: 'Registre-se'
+      });
+    }
+  }  
+
+  return (
+    <FormLoginWrapper>
+      <form id="formLogin">
+        <div id="divInputsLogin">
+          <input
+            id="inputLoginName"
+            type="text"
+            onChange={ getName }
+            placeholder="Digite seu nome"
+          />
+          <input
+            id="inputLoginEmail"
+            type="email"
+            onChange={ getEmail }
+            placeholder="Digite seu email"
+          />
+          <input
+            id="inputLoginPassword"
+            type="password"
+            onChange={ getPassword }
+            placeholder="Digite uma senha"
+          />
+        </div>
+        <div id="divButtonEnterRegister">
+          <input id="buttonFormLogin" type="submit" value={ inputSubmit.valueSubmit } />
+          <span id="textRegistreSe"><a onClick={ setValueSubmit }>{ inputSubmit.valueTxtLink }</a></span>
+        </div>
+      </form>
+    </FormLoginWrapper>
+  );
+}
