47cde278f64b99bfc721a12ebdcbf21e88cc6427
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-02 20:59:20 -0300

    feat: created function "checkToken" in file "./src/pages/Login" inserted "routers"

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,30 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 import Header from '../components/Header';
 import FormLogin from '../components/FormLogin';
+import ButtonComeBack from '../components/ButtonComeBack';
 
 import { LogineWrapper } from '../styles/pages/Login';
 
 export default function Login() {
+  const router = useRouter();
+
+  const checkToken = () => {
+    const token = localStorage.getItem('Token');
+    if (token) {
+      router.push('/');
+    }
+  }
+
+  useEffect(() => {
+    checkToken();
+  }, []);
+  
   return (
     <LogineWrapper>
       <Header />
+      <ButtonComeBack />
       <FormLogin />
     </LogineWrapper>
   );
