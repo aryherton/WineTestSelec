53139375413f4478dd128e8f81d9ee365173da70
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-02 01:53:15 -0300

    feat: created function "Search" in file "./src/components/Search.tsx"

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { SearchWrapper } from '../styles/components/Search';
 
+import { setFilterArrProducts } from '../store/slice/productSlice';
+import { setPages } from '../store/slice/productSlice';
+import { getArrAll } from '../services/api';
+
+import IProduct from '../interface/IProduct';
+
+
 export default function Search() {
   const [searchName, setSearchName] = useState('');
+  const [arrAll, setArrAll] = useState<IProduct[]>([]);
+  const dispatch = useDispatch();
 
-  const getValueSearch = ({target: { value }}) => {
-    setSearchName(value);
-  }  
+  const getValueSearch = async ({target: { value }}) => {
+    console.log(searchName.length);
+    
+    if (value) {
+      setSearchName(value);
+    } if (!value && searchName.length < 2) {
+      setSearchName('');
+      dispatch(setPages(1));
+    }
+  };
+
+  const getArr = useCallback(async () => {
+    const arr = await getArrAll();
+    
+    setArrAll(arr);
+  }, []);
+  
+
+  useEffect(() => {
+    getArr();
+
+    if (arrAll && searchName) {
+      const arrFilter = arrAll.filter(({ name }) => name.includes(searchName));
+
+      dispatch(setFilterArrProducts(arrFilter));
+    } else {
+      dispatch(setFilterArrProducts([]));
+    }
+  }, [searchName]);
 
   return (
     <SearchWrapper id="inputSearch">
