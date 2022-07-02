9517d5ebe86b1b50c7d3c317f22326f8bc494dd7
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-02 12:12:52 -0300

    fix: import "Loading" in file "./src/components/CardProduct.tsx"

--- src/components/CardProduct.tsx
+++ src/components/CardProduct.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
-// import Loading from './Loading';
+import Loading from './Loading';
 import { CardProductWrapper } from '../styles/components/CardProduct';
 import ButtonPagination from '../components/ButtonPagination';
 import IProduct from '../interface/IProduct';
@@ -28,40 +28,40 @@ export default function CardProduct() {
       <p>49 produtos encontrados</p>
       <div id="mainProduct">
         {arrProd.length
-          && (arrProd.map((product, key) => {
-          return (
-            <div id="cardProduct" key={product.id}>
-              <div id="dataPorduct">
-                <div id="imgWineFlag">
-                  <div id="imgWines">
-                    <img src={product.image} alt="img_wine" />
+          ? (arrProd.map((product: IProduct, key) => {
+            return (
+              <div id="cardProduct" key={product.id}>
+                <div id="dataPorduct">
+                  <div id="imgWineFlag">
+                    <div id="imgWines">
+                      <img src={product.image} alt="img_wine" />
+                    </div>
+                    <div id="flag">
+                        <img src={product.flag} alt="flag_country" />
+                    </div>
                   </div>
-                  <div id="flag">
-                      <img src={product.flag} alt="flag_country" />
+                  <div id="nameWine">
+                      <p>{product.name}</p>
+                  </div>
+                  <div id="valuePrice">
+                      <span id="price">R${product.price}</span>
+                      <span id="discount">{product.discount}% OFF</span>
+                  </div>
+                  <div id="textSocioPrice">
+                      <span id="textSocio">SÓCIO WINE</span>
+                      <span id="priceMember">R$ {product.priceMember}</span>
+                  </div>
+                  <div id="textNonMemberPrice">
+                      <span id="nonMember">NÃO SÓCIO</span>
+                      <span id="priceNonMember">R$ {product.priceNonMember}</span>
                   </div>
                 </div>
-                <div id="nameWine">
-                    <p>{product.name}</p>
-                </div>
-                <div id="valuePrice">
-                    <span id="price">R${product.price}</span>
-                    <span id="discount">{product.discount}% OFF</span>
-                </div>
-                <div id="textSocioPrice">
-                    <span id="textSocio">SÓCIO WINE</span>
-                    <span id="priceMember">R$ {product.priceMember}</span>
-                </div>
-                <div id="textNonMemberPrice">
-                    <span id="nonMember">NÃO SÓCIO</span>
-                    <span id="priceNonMember">R$ {product.priceNonMember}</span>
+                <div id="divButton">
+                  <input id="buttonCard" type="button" value="ADICIONAR" />
                 </div>
               </div>
-              <div id="divButton">
-                <input id="buttonCard" type="button" value="ADICIONAR" />
-              </div>
-            </div>
-          );
-        }))}
+            );
+        })): <Loading /> }
         <ButtonPagination />
       </div>
     </CardProductWrapper>
