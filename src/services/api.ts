ed455360897617a4ffa3282560c4cabd4d9f31f4
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-01 21:22:54 -0300

    feat: created function "getArrAll" in file "./src/services/api.ts"

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+import GetEndPoint from '../services/endPoint';
+
 const api = axios.create({
   baseURL: `http://wine-back-test.herokuapp.com`,
 });
@@ -8,7 +10,24 @@ export const getArr = async (endpoint: string) => {
   const data = await api.get(endpoint)
     .then((resp) => resp.data);
 
-    return data;
+  return data;
+};
+
+export const getArrAll = async () => {
+  const endPoint = GetEndPoint(1);
+  const data = await getArr(endPoint);
+  const { totalPages } = data;
+  const arrEndPoint: string[] = [];
+  
+  for (let index = 0; index < totalPages; index += 1) {
+    arrEndPoint.push(GetEndPoint(index + 1));
+  }
+
+  const arrProductsAll = await Promise.all(arrEndPoint
+      .map((endPoint: string) => getArr(endPoint))
+    );
+  
+  return arrProductsAll.reduce((acc, { items }) => acc = [...acc, ...items], []);
 };
 
 export default api;
