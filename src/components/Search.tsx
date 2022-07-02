5183df2ab730df6ffd3a82260440f040fc6bd7a4
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-01 22:05:37 -0300

    feat: inserted import "useState" in file "./src/compnents/Search.tsx"

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,9 +1,17 @@
+import React, { useState } from 'react';
+
 import { SearchWrapper } from '../styles/components/Search';
 
 export default function Search() {
+  const [searchName, setSearchName] = useState('');
+
+  const getValueSearch = ({target: { value }}) => {
+    setSearchName(value);
+  }  
+
   return (
     <SearchWrapper id="inputSearch">
-      <input type="text" placeholder="Pesquise pelo nome"/>
+      <input type="text" placeholder="Pesquise pelo nome" onChange={ getValueSearch }/>
     </SearchWrapper>
   );
 }
