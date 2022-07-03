fec235e243a2be02768abe239ce052b4171e1221
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-03 01:38:24 -0300

    feat: created file "MainPageProduct.ts" in folder "./src/styles/components"

---
+++ src/styles/components/MainPageProduct.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const MainPageProductWrapper = styled.main`
+  width: 100%;
+  height: 100vh;
+
+  section {
+    width: 50%;
+    height: 60%;
+    padding-top: 75px;
+  }
+`;
