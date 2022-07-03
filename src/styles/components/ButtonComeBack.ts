cda2289ba2971eb05676baee61a51e55f6506085
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-03 01:07:58 -0300

    feat: change size "height" in file "./src/components/ButtonComeBack.ts"

--- src/styles/components/ButtonComeBack.ts
+++ src/styles/components/ButtonComeBack.ts
@@ -2,17 +2,16 @@ import styled from 'styled-components';
 
 export const ButtonComeBackWrapper = styled.div`
   width: 100%;
-  height: 130px;
-  padding-top: 30px;
+  height: 60px;
+  padding-top: 40px;
 
   a {
     width: 100%;
     height: 100%;
     padding-left: 130px;
     display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: space-evenly;
+    align-items: center;
     cursor: pointer;
+    font-size: 30px;
   }
 `;
