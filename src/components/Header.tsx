e5286794714efb0fbc63f7d2cde831105afdf05f
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-03 17:50:54 -0300

    feat: insertede hook "useEffect" in file "./src/components/Header.tsx"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,22 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
 import Image from 'next/image';
 
 import NavBar from './NavBar';
 import MenuTop from './MenuTop';
 import { HeaderWrapper } from '../styles/components/Header';
 import Logo from '../images/logo.png';
+import productSlice from '../store/slice/productSlice';
 
 export default function Header() {
+  const qtdStorageCart = useSelector((state: any) => state.products.lengthCart);
+
+  useEffect(() => {
+    console.log('test');
+    
+  }, [qtdStorageCart]);
+
   return (
     <HeaderWrapper>
       <div id="logo_wine">
