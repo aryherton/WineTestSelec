5377ae78da666b13d66e79a2cf50ee26b33e24d4
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-03 19:29:56 -0300

    feat: change name "ICard" for "ICart" in file "./src/services/CartProduct.tsx"

--- src/services/CartProduct.tsx
+++ src/services/CartProduct.tsx
@@ -1,16 +1,16 @@
-import ICard from '../interface/ICard';
+import ICart from '../interface/ICart';
 import { useDispatch } from 'react-redux';
 
 import { setLengthCart } from '../store/slice/productSlice';
 
 export const AddProductInCard = (idP: number) => {
   const dispatch = useDispatch();
-  let arrIdProduct: ICard[] = [];
+  let arrIdProduct: ICart[] = [];
   const getCart = localStorage.getItem('Cart');
 
   if (getCart) {
     arrIdProduct = JSON.parse(getCart);
-    const arrTmp = arrIdProduct.reduce((acc: ICard[], product: ICard, _index: number, arr: ICard[]) => {
+    const arrTmp = arrIdProduct.reduce((acc: ICart[], product: ICart, _index: number, arr: ICart[]) => {
       if (+product.idProduct === +idP) {
         product.qtd = product.qtd + 1;
         acc.push(product);
