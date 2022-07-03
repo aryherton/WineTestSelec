b355aa2fa976b59669ff59cc6142e54659678449
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-02 21:02:06 -0300

    feat: created var global "URL_REGISTER_USER" in file "./src/services/endPoint.ts"

--- src/services/endPoint.ts
+++ src/services/endPoint.ts
@@ -1,3 +1,7 @@
+export const URL_LOGIN_DB_WINE = 'http://localhost:3005/login';
+export const URL_REGISTER_USER = 'http://localhost:3005/login/register';
+export const URL_PRODUCT_DB_WINE = 'http://localhost:3005/product';
+
 export default function GetEndPoint(pages: number) {
   return `/products?page=${pages}&limit=10`;
 }
