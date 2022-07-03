7fcaf50c6e3ef4ce3793d8424f1fa020a11c00a5
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-03 00:54:29 -0300

    feat: created file "ButtonAdd.tsx" in folder "./src/components"

---
+++ src/components/ButtonAdd.tsx
@@ -0,0 +1,12 @@
+export default function ButtonAdd() {
+  return (
+    <div id="buttonPageProduct">
+      <div id="addSubProduct">
+        <button id="imgAdd"> - </button>
+        <span id="imgQtdProduct">0</span>
+        <button id="imgSub"> + </button>
+      </div>
+      <button id="buttonAdd">Adicionar</button>
+    </div>
+  );
+}
