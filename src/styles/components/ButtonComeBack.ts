d5410086c1f2d4f4d381fa6864e6dffcbdedd9ae
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-02 21:03:14 -0300

    feat: created file "ButtonComeBaack.ts" in folder "./src/styles/components"

---
+++ src/styles/components/ButtonComeBack.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const ButtonComeBackWrapper = styled.div`
+  width: 100%;
+  height: 130px;
+  padding-top: 30px;
+
+  a {
+    width: 100%;
+    height: 100%;
+    padding-left: 130px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-evenly;
+    cursor: pointer;
+  }
+`;
